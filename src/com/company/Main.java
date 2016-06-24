70b0fe0f15336225fa30f37e5c5c1387e48febfc
Jenia <gr.yevhen@gmail.com>
2016-06-24 16:57:36 +0300

    Modify main, cut creating threads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,15 @@ public class Main {
         Array leftSum = new Array(array, 0, array.length/2);
         Array rightSum = new Array(array, array.length/2, array.length);
 
-        Thread leftThread = new Thread(leftSum);
-        Thread rightThread = new Thread(rightSum);
-
-        leftThread.start();
-        rightThread.start();
-
-        try{
-            leftThread.join();
-            rightThread.join();
+        leftSum.start();
+        rightSum.start();
+        try{;
+            leftSum.join();
+            rightSum.join();
         }catch (InterruptedException e){
         }
 
-        int left = leftSum.getSum();
-        int right = rightSum.getSum();
-        System.out.println(left + right);
+        System.out.println(leftSum.getSum() + rightSum.getSum());
 
     }
 }
